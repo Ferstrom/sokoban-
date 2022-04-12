37c8a4b94cb2a0044b9304d43d39cc3013a02896
LUIS FERNANDO FERNANDO <97562449+Ferstrom@users.noreply.github.com>
2022-04-12 02:27:43 +0000

    Meta,muñeco abajo

--- sokoban.py
+++ sokoban.py
@@ -210,7 +210,7 @@ class Sokoban:
             self.mapa[self.muneco_fila][self.muneco_columna] = 3
             self.mapa[self.muneco_fila + 1][self.muneco_columna] = 2
             self.muneco_fila += 1
-        #Meta,personaje
+        #Meta,muñeco
         elif self.mapa[self.muneco_fila][self.muneco_columna] == 2 and self.mapa[self.muneco_fila + 1][self.muneco_columna] == 4:
             self.mapa[self.muneco_fila][self.muneco_columna] = 3
             self.mapa[self.muneco_fila + 1][self.muneco_columna] = 5
@@ -220,7 +220,7 @@ class Sokoban:
             self.mapa[self.muneco_fila][self.muneco_columna] = 3
             self.mapa[self.muneco_fila + 1][self.muneco_columna] = 2
             self.mapa[self.muneco_fila + 2][self.muneco_columna] = 0
-            self.muneco_fila += 1
+            self.muneco_fila -= 1
     
         
 
