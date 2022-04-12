dae8f45402457c04562ca4fccefe5e678f3aec87
LUIS FERNANDO FERNANDO <97562449+Ferstrom@users.noreply.github.com>
2022-04-12 20:51:29 +0000

    Muñeco_meta,caja_meta,meta Arriba

--- sokoban.py
+++ sokoban.py
@@ -338,10 +338,13 @@ class Sokoban:
             self.mapa[self.muneco_fila - 1][self.muneco_columna] = 5
             self.mapa[self.muneco_fila - 2][self.muneco_columna] = 0
             self.muneco_fila -= 1
+        #Muñeco_meta,caja_meta,meta
+        elif self.mapa[self.muneco_fila][self.muneco_columna] == 5 and self.mapa[self.muneco_fila - 1][self.muneco_columna] == 6 and self.mapa[self.muneco_fila - 2][self.muneco_columna] == 4:
+            self.mapa[self.muneco_fila][self.muneco_columna] = 4
+            self.mapa[self.muneco_fila - 1][self.muneco_columna] = 5
+            self.mapa[self.muneco_fila - 2][self.muneco_columna] = 6
+            self.muneco_fila -= 1
         
-
-       
-            
     def jugar(self):
         while True:
             self.imprimirMapa()
